ea5bc54ca39dc5174b1f8682f1ab0b0cb21896a6
Roberto Vázquez Hasting <robertovazquezhasting@MacBook-Air-de-Roberto.local>
2022-07-25 14:38:33 -0500

    prime commit

---
+++ src/components/Modal.jsx
@@ -0,0 +1,32 @@
+import CerrarBtn from '../img/cerrar.svg'
+
+const Modal = ({setModal, animarModal, setAnimarModal}) => {
+
+    const oculatModal = () => {
+        
+        setAnimarModal(false)
+
+        setTimeout(() => {
+            setModal(false)
+        }, 500);
+    }
+
+    return (
+        <div className="modal">
+            <div className="cerrar-modal">
+                <img
+                    src={CerrarBtn}
+                    alt='Cerrar Modal'
+                    onClick={oculatModal}
+                />
+            </div>
+
+            <form className={`formulario ${animarModal ? "animar" : "cerrar"}`}>
+                <legend>Nuevo Gasto</legend>
+            </form>
+
+        </div>
+    )
+}
+
+export default Modal
---
+++ src/components/NuevoPresupuesto.jsx
@@ -0,0 +1,55 @@
+import { useState } from 'react'
+import Mensaje from './Mensaje'
+
+const NuevoPresupuesto = ({
+  presupuesto, setPresupuesto, 
+  setIsValidPresupuesto
+}) => {
+
+  const [mensaje, setMensaje] = useState('')
+
+  const handlePresupuesto = (e) => {
+    e.preventDefault()
+
+/*     if (!Number(presupuesto) || Number(presupuesto) < 0){
+      setMensaje('No es un presupuesto válido')
+      return
+    }  */
+
+    if (!presupuesto || presupuesto < 0){
+      setMensaje('No es un presupuesto válido')
+      return
+    } 
+
+    setMensaje('')
+    setIsValidPresupuesto(true)
+  }
+
+  return (
+    <div className='contenedor-presupuesto contenedor sombra'>
+      
+      <form onSubmit={handlePresupuesto} className='formulario'>
+        <div className='campo'>
+          <label>Definir Presupuesto</label>
+          <input
+           className='nuevo-presupuesto' 
+           type="number"
+           placeholder="Añade tu presupesto"
+           value={presupuesto}
+           onChange={ e => setPresupuesto(Number(e.target.value))}
+          />
+        </div>
+
+        <input type="submit" value="Añadir"/>
+
+        {mensaje && <Mensaje tipo="error">{mensaje}</Mensaje>}
+
+      </form>
+
+
+
+    </div>
+  )
+}
+
+export default NuevoPresupuesto
